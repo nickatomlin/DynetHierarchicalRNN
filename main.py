99ed1eb9e7b7b9dc70e801035f866157e554a2aa
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2018-08-07 19:11:29 -0700

    Convert data to int lists

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import json
 from sklearn.model_selection import train_test_split
 from tensorflow.python.layers.core import Dense
 
+vocab = ['<PAD>', '$UNK', '<START>', '<END>', 'a', 'b']
+
 def get_random_string(length):
 	"""
 	Get a random "ab"-string with number of characters equal to "length"
@@ -35,7 +37,7 @@ def flip_string(string):
 
 
 
-def translate_example(num_examples=1024, test_size=4):
+def translate_example(num_examples=10, test_size=4):
 	"""
 	Translate operation:
 	 - All dialogues length two
@@ -43,21 +45,26 @@ def translate_example(num_examples=1024, test_size=4):
 
 	E.g., ["abbba", "baaab"]
 	"""
-	vocab = ['<PAD>', '$UNK', '<START>', '<END>', 'a', 'b']
 
 	data = []
 	for i in range(num_examples):
 		first_string = get_random_string(5)
 		second_string = flip_string(first_string)
-		encoder_input = ["", first_string]
-		decoder_input = [first_string, second_string]
+		encoder_input = [prepare_data("<PAD>"), prepare_data(first_string)]
+		decoder_input = [prepare_data(first_string), prepare_data(second_string)]
 		data.append((encoder_input, decoder_input))
 
 	train_data, test_data = train_test_split(data, test_size=test_size)
 	X, y = zip(*train_data)
 	X_test, y_test = zip(*test_data)
 	
-	print(y_test)
+
+def prepare_data(string):
+	vals = string.split()
+	output = []
+	for val in vals:
+		output.append(vocab.index(val))
+	return output
 
 if __name__ == '__main__':
 	translate_example()
